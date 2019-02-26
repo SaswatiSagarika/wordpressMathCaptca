ca6a61cf5a4000b2c8d8b856fe3bd666d326addb
Saswati <saswatisagarika@gmail.com>
2019-02-26 13:16:42 +0530

    math captcha plugin coding

---
+++ add-captcha-plugin.php
@@ -0,0 +1,115 @@
+<?php
+/**
+* Plugin Name: Add Captcha
+* Plugin URI: http://wp.test/plugin
+* Description: This plugin is used to add math captcha in the wp-login page
+* Version: 1.0
+* Author: Saswati
+* Author URI: http://wp.test/
+* Text Domain: addcaptcha-plugin
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+define( 'ADD_CAPTCHA_URL', plugins_url( '', __FILE__ ) );
+define( 'ADD_CAPTCHA_PATH', plugin_dir_path( __FILE__ ) );
+
+class AddCaptchaPlugin {
+
+     /**
+     * registering the actions
+     */
+    function register() {
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+        add_action( 'login_form', array( $this, 'addedToLogin' ) );
+        add_filter( 'authenticate', array( $this, 'customAuthenticate' ), 10, 3 );
+
+    }
+
+
+    /**
+     * Activation.
+     */
+    public function activation() {
+
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Deactivation.
+     */
+    public function deactivation() {
+
+        flush_rewrite_rules();
+    }
+
+    /**
+     * enqueuing the css file.
+     */
+    public function enqueue() {
+
+        wp_enqueue_style( 'captcha-css', ADD_CAPTCHA_URL.'/assests/css/style.css',  array(), false, 'all' );
+    
+    }
+
+    /**
+     * add the captcha part to the login page
+     */
+    public function addedToLogin() {
+        session_start();
+        $digit1 = mt_rand( 1, 20 );
+        $digit2 = mt_rand( 1, 20 );
+        if( mt_rand( 0, 1 ) === 1 ) {
+            $math = "$digit1 + $digit2";
+            $_SESSION['answer'] = $digit1 + $digit2;
+        } else {
+            $math = "$digit1 - $digit2";
+            $_SESSION['answer'] = $digit1 - $digit2;
+        }
+        ?>
+            <p>
+                <label for="answer">Enter the captcha value</label><br>
+                <?php echo $math; ?> = <input name="answer" type="text" />
+            </p>
+        <?php
+    }
+
+     /**
+     * login the user after validating the captcha answer
+     */
+    public function customAuthenticate( $user, $username, $password ){
+        session_start();
+
+        //Get POSTED value
+        $my_value = $_POST['answer'];
+        //Get user object
+        $user = get_user_by( 'login', $username );
+
+        //Get stored value
+            $stored_value = $_SESSION['answer'];
+
+        if(!$user || empty($my_value) || $my_value != $stored_value){
+            //User note found, or no value entered or doesn't match stored value - don't proceed.
+                remove_action( 'authenticate', 'wp_authenticate_username_password', 20 );
+                remove_action( 'authenticate', 'wp_authenticate_email_password', 20 ); 
+
+            //Create an error to return to user
+                return new WP_Error( 'denied', __( "<strong>ERROR</strong>: You're unique identifier was invalid." ) );
+        }
+
+        //Make sure you return null 
+        return null;
+    }
+}
+
+if ( class_exists( 'AddCaptchaPlugin' ) ) {
+
+    $addCaptchaPlugin = new AddCaptchaPlugin();
+    $addCaptchaPlugin->register();
+}
+
+register_activation_hook( __FILE__, array( $addCaptchaPlugin, 'activation' ) );
+register_deactivation_hook( __FILE__, array( $addCaptchaPlugin, 'deactivation' ) );
